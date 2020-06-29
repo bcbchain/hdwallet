196ab835c07247ae1f3e5af75a0596de1e8188f8
godeamon <122943094@qq.com>
2020-06-29 15:15:30 +0800

    update scripts for packing

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -6,12 +6,20 @@ XC_OS=${XC_OS:-"darwin linux windows"}
 
 echo "==> Building..."
 cd ..
+rm -rf build
+mkdir -p build/dist
 
 IFS=' ' read -ra arch_list <<< "$XC_ARCH"
 IFS=' ' read -ra os_list <<< "$XC_OS"
 for arch in "${arch_list[@]}"; do
 	for os in "${os_list[@]}"; do
 		echo "--> $os/$arch"
-		GOOS=${os} GOARCH=${arch} go build -tags="${BUILD_TAGS}" -o "build/pkg/${os}_${arch}/pieces/bcchain" ./cmd/...
+		mkdir -p build/bin/"${os}_${arch}"
+		GOOS=${os} GOARCH=${arch} go build -tags="${BUILD_TAGS}" -o "build/bin/${os}_${arch}" ./cmd/...
+
+		pushd "build/bin/${os}_${arch}" > /dev/null || exit 1
+		pwd
+    tar -zcvf ../../dist/$project_name\_$VERSION\_${os}_${arch}.tar.gz ./*
+    popd > /dev/null || exit 1
 	done
 done
